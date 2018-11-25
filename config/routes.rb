551891ed270d69127123c100b5151780548918ed
takuya-teramoto <t.teramon23@gmail.com>
2018-11-25 15:17:08 +0900

    create edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'prototypes#index'
 
   resources :prototypes do
-    resources :comments, only: [:create, :update, :destroy, :index]
+    resources :comments, only: [:create, :update, :destroy, :edit, :index]
   end
   resources :users, only: [:show, :edit, :update]
   resources :tags, only: [:show, :index]
